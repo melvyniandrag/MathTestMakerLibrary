235d156cfba06d966609914d23280ae9d63f71ed
melvyniandrag <melvyniandrag@gmail.com>
2020-10-19 01:31:49 -0400

    added arg parser to help remember how to use the pdfmaker script

--- pdfmaker/pdfmaker.py
+++ pdfmaker/pdfmaker.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
 
-import sys
-import jinja2
-import os
 from jinja2 import Template
+import argparse
+import jinja2
 import json
+import os
 import subprocess
+import sys
 
-templateDir = "/home/melvyn/Desktop/MathTestMakerLibrary/pdfmaker"
-OUTPUT_DIR="/tmp"
-JOB_NAME=sys.argv[1]
+parser = argparse.ArgumentParser(description='Generate a pdf from json passed to stdin.')
+parser.add_argument('posargs', metavar='posargs', type=str, nargs='+',
+                    help='[job name, output directory]. job name will be the name of the generated pdf.')
+parser.add_argument('infile', metavar='json stdin', nargs='?', type=argparse.FileType('r'),
+					default=sys.stdin, help="json file passed to stdin via <")
+args = parser.parse_args()
+
+JOB_NAME=args.posargs[0]
+OUTPUT_DIR=args.posargs[1]
+
+TEMPLATE_DIR = "/home/melvyn/Desktop/MathTestMakerLibrary/pdfmaker"
+TEMPLATE = "test-template001.tex"
 
 latex_jinja_env = jinja2.Environment(
 	block_start_string = '\BLOCK{',
@@ -22,11 +32,13 @@ latex_jinja_env = jinja2.Environment(
 	line_comment_prefix = '%#',
 	trim_blocks = True,
 	autoescape = False,
-	loader = jinja2.FileSystemLoader(templateDir)
+	loader = jinja2.FileSystemLoader(TEMPLATE_DIR)
 )
-template = latex_jinja_env.get_template('test-template001.tex')
+
+template = latex_jinja_env.get_template(TEMPLATE)
 test_data = json.loads(sys.stdin.read())
 tex_data = template.render(test_data)
 with open(JOB_NAME+".tex", "w") as fout:
 	fout.write(tex_data)
+
 subprocess.run(["pdflatex", "-output-directory=" + OUTPUT_DIR, JOB_NAME + ".tex"])
